c72d979a5df34079d6e1cc403687e703b35457cc
n.zare <n.zare@behsacorp.com>
2024-04-02 16:53:57 +0330

    first commit

---
+++ src/test/java/com/behsa/webclient/WebclientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.behsa.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
